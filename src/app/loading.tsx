6d017efd4ed481d3655b70b0231d78542404c5d7
Mehedi <mehedihasan712277@gmail.com>
2026-08-28 20:19:04 +0600

    fix: loading position

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <main className="min-h-screen w-full flex justify-between items-center bg-background ">
+        <main className="min-h-screen w-full flex justify-center items-center bg-background ">
             <p>Loading...</p>
         </main>
     );
